1833359cc63895d4bb91baf47e84657e58496bd2
大黄耗子 <5453811+keyframe@user.noreply.gitee.com>
2021-02-15 20:34:27 +0800

    format

---
+++ js/formatDate.js
@@ -0,0 +1,28 @@
+//  原生js格式化时间
+function formatDate(time, format = "YY-MM-DD hh:mm:ss") {
+  var date = new Date(time);
+
+  var year = date.getFullYear(),
+    month = date.getMonth() + 1, //月份是从0开始的
+    day = date.getDate(),
+    hour = date.getHours(),
+    min = date.getMinutes(),
+    sec = date.getSeconds();
+  var preArr = Array.apply(null, Array(10)).map(function (elem, index) {
+    return "0" + index;
+  }); //开个长度为10的数组 格式为 ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09"]
+
+  var newTime = format
+    .replace(/YY/g, year)
+    .replace(/MM/g, preArr[month] || month)
+    .replace(/DD/g, preArr[day] || day)
+    .replace(/hh/g, preArr[hour] || hour)
+    .replace(/mm/g, preArr[min] || min)
+    .replace(/ss/g, preArr[sec] || sec);
+
+  return newTime;
+}
+
+alert(formatDate(new Date().getTime())); //2019-02-21 10:45:54
+alert(formatDate(new Date().getTime(), "YY年MM月DD日")); //2019年02月21日
+alert(formatDate(new Date().getTime(), "今天是YY/MM/DD hh:mm:ss")); //今天是2019/02/21 10:49:29
